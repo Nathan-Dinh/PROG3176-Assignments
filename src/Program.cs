630d90d8bf9408baab66058d8cdf61ecdc1aa8e0
Nathan Dinh <nathan.dinh03@outlook.com>
2026-01-31 00:15:38 -0500

    Init
    This is the initial commit for this assignment

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+namespace src.assignment_1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
